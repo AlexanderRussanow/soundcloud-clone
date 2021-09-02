b2146f70990feb024a808195c265aa3ddff4e1c9
Alexander Russanow <alexander.russanow@gmail.com>
2021-09-02 21:52:04 +0200

    first commit

---
+++ server/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
+import { FileModule } from './files/file.module';
+import { TrackModule } from './track/track.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.bht6o.mongodb.net/soundcloud-clone?retryWrites=true&w=majority'),
+    FileModule,
+    TrackModule
+  ],
+})
+export class AppModule {}
